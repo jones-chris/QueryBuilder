f704b96d47afb40d1b98d02225b44f5f7ad57922
christopheralan88 <christopheralan88@gmail.com>
2018-02-22 19:52:20 -0500

    Adding more tests for DatabaseConnectionFactoryTests and DatabaseConnectionTests.

--- QueryBuilderTests/Config/DatabaseConnectionFactoryTests.cs
+++ QueryBuilderTests/Config/DatabaseConnectionFactoryTests.cs
@@ -14,10 +14,76 @@ namespace QueryBuilder.Config.Tests
     public class DatabaseConnectionFactoryTests
     {
         [TestMethod]
-        public void DatabaseConnectionFactory_GoodDatabaseTypeAndConnectionString()
+        public void DatabaseConnectionFactory_PostgreSQL()
         {
             var databaseType = DatabaseType.PostgreSQL;
             var connString = "User ID=root;Password=myPassword;Host=localhost;Port=5432;Database=myDataBase;Pooling = true;";
+
+            var databaseConnection = new DatabaseConnectionFactory().CreateDbConnection(databaseType, connString);
+
+            Assert.IsTrue(databaseConnection != null);
+        }
+
+        [TestMethod]
+        public void DatabaseConnectionFactory_Access()
+        {
+            var databaseType = DatabaseType.Access;
+            var connString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\mydatabase.accdb;User Id=admin;";
+
+            var databaseConnection = new DatabaseConnectionFactory().CreateDbConnection(databaseType, connString);
+
+            Assert.IsTrue(databaseConnection != null);
+        }
+
+        [TestMethod]
+        public void DatabaseConnectionFactory_MySQL()
+        {
+            var databaseType = DatabaseType.MySql;
+            var connString = "Server=myServerAddress;Database=myDataBase;Uid=myUsername;Pwd=myPassword;";
+
+            var databaseConnection = new DatabaseConnectionFactory().CreateDbConnection(databaseType, connString);
+
+            Assert.IsTrue(databaseConnection != null);
+        }
+
+        [TestMethod]
+        public void DatabaseConnectionFactory_Oracle()
+        {
+            var databaseType = DatabaseType.Oracle;
+            var connString = "Data Source=urOracle;User Id=urUsername;Password=urPassword;DBA Privilege=SYSDBA;";
+
+            var databaseConnection = new DatabaseConnectionFactory().CreateDbConnection(databaseType, connString);
+
+            Assert.IsTrue(databaseConnection != null);
+        }
+
+        [TestMethod]
+        public void DatabaseConnectionFactory_Redshift()
+        {
+            var databaseType = DatabaseType.Redshift;
+            var connString = "Server=server; Database=database;UID=user;PWD=password;Port=port;SSL=true;Sslmode=Require";
+
+            var databaseConnection = new DatabaseConnectionFactory().CreateDbConnection(databaseType, connString);
+
+            Assert.IsTrue(databaseConnection != null);
+        }
+
+        [TestMethod]
+        public void DatabaseConnectionFactory_SQLite()
+        {
+            var databaseType = DatabaseType.Sqlite;
+            var connString = "Data Source=c:\\mydb.db;Version=3;";
+
+            var databaseConnection = new DatabaseConnectionFactory().CreateDbConnection(databaseType, connString);
+
+            Assert.IsTrue(databaseConnection != null);
+        }
+
+        [TestMethod]
+        public void DatabaseConnectionFactory_SQLServer()
+        {
+            var databaseType = DatabaseType.SqlServer;
+            var connString = "Server=myServerAddress;Database=myDataBase;Trusted_Connection=True;";
 
             var databaseConnection = new DatabaseConnectionFactory().CreateDbConnection(databaseType, connString);
 
---
+++ QueryBuilderTests/DatabaseConnection/DatabaseConnectionTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QueryBuilder.DatabaseConnections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QueryBuilder.Config;
+using System.Data;
+
+namespace QueryBuilder.DatabaseConnections.Tests
+{
+    [TestClass]
+    public class DatabaseConnectionTests
+    {
+        public static DatabaseType dbType = DatabaseType.Access;
+        public static string connString = "Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=C:\\novena\\db_testing\\novena_dev.accdb;";
+        public IDatabaseConnection dbAccess = new DatabaseConnectionFactory().CreateDbConnection(dbType, connString);
+
+        [TestMethod]
+        public void query_Access()
+        {
+            var sql = "select top 10 * from cash_contributions_detail;";
+
+            var dt = dbAccess.query(sql);
+
+            Assert.IsTrue(dt.Rows.Count == 10);
+        }
+
+        [TestMethod]
+        public void userSignInTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void getSchemaTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
